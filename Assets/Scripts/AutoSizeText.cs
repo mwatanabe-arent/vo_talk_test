5e6f85303aee124ed6877f53c20ac073ff6ea22e
mwatanabe_arent <m.watanabe@arent3d.com>
2023-05-29 17:20:18 +0900

    プロジェクト追加

---
+++ Assets/Scripts/AutoSizeText.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+[RequireComponent(typeof(RectTransform))]
+public class AutoSizeText : MonoBehaviour
+{
+    private TextMeshProUGUI textComponent;
+    private RectTransform rectTransform;
+
+    private void Awake()
+    {
+        textComponent = GetComponent<TextMeshProUGUI>();
+        rectTransform = GetComponent<RectTransform>();
+    }
+    private void OnValidate()
+    {
+        //var textGenerator = new TextGenerator();
+        //var textGenerationSettings = textComponent.GetGenerationSettings(rectTransform.rect.size);
+
+    }
+    private void Update()
+    {
+        var textInfo = textComponent.GetTextInfo(textComponent.text);
+
+        var width = textInfo.textComponent.preferredWidth;
+        var height = textInfo.textComponent.preferredHeight;// textGenerator.GetPreferredHeight(textComponent.text, textGenerationSettings);
+
+        rectTransform.sizeDelta = new Vector2(width, height);
+
+    }
+}
